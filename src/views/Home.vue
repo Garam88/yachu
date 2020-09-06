ccc328e6603ebe81f424831dee409c740546e3cc
Garam Kim <grm.kim@sk.com>
2020-09-06 16:30:59 +0900

    home 작업 시작

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
-
+    <div class="game-area">
+      <div class="score-area">
+        <div class="table">
+          <div class="row" v-for="score in scoreType">
+            <div class="column title">
+              {{ score.title }}
+            </div>
+            <div class="column">
+              {{ score.num }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="dice-area">
+        dice
+      </div>
+    </div>
+    <div class="inf-area">
+      interface
+    </div>
   </div>
 </template>
 
@@ -9,6 +28,50 @@ import {Vue, Component} from 'vue-property-decorator'
 
 @Component
 export default class Home extends Vue {
-
+  scoreType = [{title:'Aces', num: 0}, {title:'Deuces', num: 0}, {title:'Threes', num: 0}, {title:'Fours', num: 0}, {title:'Fives', num: 0}, {title:'Sixes', num: 0}]
 }
 </script>
+
+<style scoped="scoped" lang="less">
+.home {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+
+  .game-area {
+    width: 1000px;
+
+    .score-area {
+      height: 600px;
+
+      .table {
+        width: 200px;
+
+        .column.title {
+          width: 100px;
+        }
+      }
+    }
+  }
+  .inf-area {
+    display: flex;
+    flex: 1 auto;
+  }
+
+  .table {
+    display: flex;
+    flex-direction: column;
+
+    .row {
+      display: flex;
+      flex-direction: row;
+
+      .column {
+        &.title {
+          font-weight: bold;
+        }
+      }
+    }
+  }
+}
+</style>
